0ea3a271d8cad0ee311d6aecef9166f0c173d898
Arjun singh gurjar <115486632+Arjun09091999@users.noreply.github.com>
2023-05-18 16:18:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   let decodedArr = ""; // Your Result goes here
   // Only change code below this line
 	for(let i=0;i<encodedStr.length;i++){
 	decodedArr.push(lookup[encodedStr[i]]);
